b2c335c55fb4c5ebfaec43890c3e1ceddfecac56
Joseph Csoti <josephcsoti@gmail.com>
2019-11-07 16:40:28 -0800

    Added db file for writes/reads

---
+++ wikitrustbackend/Firestore.py
@@ -0,0 +1,34 @@
+from google.cloud import firestore
+
+class Firestore:
+
+    def __init__(self):
+      self.db = firestore.Client()
+      self.DB_COLLECTION_DIFFS = self.db.collection(u'diffs')
+      self.DB_COLLECTION_ARTICLES = self.db.collection(u'articles')
+
+    def writeDiff(self, id, data):
+      diff_ref = self.DB_COLLECTION_DIFFS.document(id)
+      diff_ref.set(data)
+
+    def readDiff(self, id):
+      diff_ref = self.DB_COLLECTION_DIFFS.document(id)
+
+      try:
+        data = diff_ref.get()
+        return data.to_dict()
+      except Exception as e:
+        return 'ERROR: ' + e
+
+    def writeArticle(self, id, data):
+      diff_ref = self.DB_COLLECTION_ARTICLES.document(id)
+      diff_ref.set(data)
+
+    def readArticle(self, id):
+      article_ref = self.DB_COLLECTION_ARTICLES.document(id)
+
+      try:
+        data = article_ref.get()
+        return data.to_dict()
+      except Exception as e:
+        return 'ERROR: ' + e
---
+++ wikitrustbackend/FirestoreTester.py
@@ -0,0 +1,28 @@
+from Firestore import Firestore
+
+# Create object
+f = Firestore()
+
+id_a = 'aaaa'
+id_b = 'bbbb'
+
+data_a = {
+    u'diff_moves': ["(A,B,C,D)", "(E,F,G,H)"],
+    u'diff_trust': [11, 22],
+    u'trust': 1
+}
+
+data_b = {
+    u'diff_moves': ["(I,J,K,L)", "(M,N,O,P)"],
+    u'diff_trust': [88, 99],
+    u'trust': 99
+}
+
+print("Writing diff a...")
+f.writeDiff(id_a, data_a)
+print("Writing diff b...")
+f.writeDiff(id_b, data_b)
+
+print(f.readDiff(id_a))
+print(f.readDiff(id_b))
+
