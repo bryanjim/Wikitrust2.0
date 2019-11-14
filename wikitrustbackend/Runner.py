c61256ba84815f7006083f6e9635c73b8246923c
Joseph Csoti <josephcsoti@gmail.com>
2019-11-14 11:23:56 -0800

    Added uploading of article text

--- wikitrustbackend/Runner.py
+++ wikitrustbackend/Runner.py
@@ -16,6 +16,16 @@ rev_list = w.get_revision_ids("Stomach", 100)  # ~97.4 [0:1]
 rev_a = w.get_revision_text(rev_list[0])
 rev_b = w.get_revision_text(rev_list[1])
 
+
+#Store article text
+article_Stomach = {
+    u"title": u"Stomach",
+    u"text": rev_a,
+}
+
+f.writeArticle(str(rev_list[0]), article_Stomach)
+art = f.readArticle(str(rev_list[0]))
+
 # Compute diff
 diff = DiffEngine.compute_edit_list(rev_a, rev_b)
 
@@ -25,15 +35,15 @@ rep = ReputationEngine.assignTrust(parsed_diff)
 
 ReputationEngine.checkVal(parsed_diff, ReputationEngine.tArray)
 
-print ReputationEngine.tArray
+#print ReputationEngine.tArray
 
-print "rep: " + str(rep)
+print("rep: " + str(rep))
 
 # Store data in db
-id = str(rev_list[0]) + str(rev_list[99])
+id = str(rev_list[0]) + str(rev_list[1])
 data = {u"diff_moves": str(diff), u"diff_trust": [1, 2, 3, 4], u"trust": rep}
 
 f.writeDiff(id, data)
 
 # make sure it stored
-print f.readDiff(id)
+print(f.readDiff(id))
