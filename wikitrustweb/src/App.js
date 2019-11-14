450c7e3f1d37f49669e596d1c8a543244565fae5
Joseph Csoti <josephcsoti@gmail.com>
2019-11-14 11:24:55 -0800

    Made webapp pull from db

--- wikitrustweb/src/App.js
+++ wikitrustweb/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 function App() {
+  const [data, setData] = useState({ documents: [], articles: []});
+  useEffect(() => {
+    const fetchData = async () => {
+      const result_a = await axios(
+        'https://firestore.googleapis.com/v1/projects/wikitrustapp/databases/(default)/documents/diffs/',
+      );
+      const result_b = await axios(
+        'https://firestore.googleapis.com/v1/projects/wikitrustapp/databases/(default)/documents/articles/',
+      );
+      setData(result_a.data);
+      setData(result_b.data);
+    };
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {data.documents.map(item => (
+        <li key={item.name}>
+          <h1>Trust: {item.fields.trust.doubleValue}</h1>
+          <p>Diff: {item.fields.diff_moves.bytesValue}</p>
+        </li>
+      ))}
+    </ul>
   );
 }
-
 export default App;
