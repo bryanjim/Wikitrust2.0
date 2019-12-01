c053db36d5c49f0062312155287e91392138f603
Joseph Csoti <josephcsoti@gmail.com>
2019-12-01 00:37:11 -0800

    Added db connection to extension

--- extension/core/wtscore.js
+++ extension/core/wtscore.js
@@ -1,4 +1,6 @@
-function colorPercentage(percentage){
+const ENDPOINT_URL = 'https://firestore.googleapis.com/v1/projects/wikitrustapp/databases/(default)/documents/articles/'
+
+let colorPercentage = (percentage) => {
     let r, g, b = 0;
     if (percentage < 50){
         r = 255;
@@ -12,19 +14,55 @@ function colorPercentage(percentage){
     return '#' + ('000000' + hex.toString(16)).slice(-6);
 }
 
-function getTrustScore(){
-    let trust = Math.floor(Math.random() * 100) + 1;
-    return trust;
+// Entry point of app
+window.onload = function(){
+    loadDummyScores();
+    loadRealScores("837852180");
 }
 
-window.onload = function(){
-    score = this.getTrustScore();
-    color = this.colorPercentage(score);
-    let gradient = this.document.getElementById('gradient')
+// Preloads ui w/ fake data
+let loadDummyScores = () => {updateUI("Loading...", "Loading...", colorPercentage(0));}
 
-    gradient.style.backgroundColor = color
+// Fetches data then updates ui
+let loadRealScores = (page) => {
+    dataPromise(page)
+        .then((res) => {
+            let trust = 101;
+            let author = 101;
+            let color = colorPercentage(trust)
+            updateUI(trust, author, color);
+            updateDebug(res);
+        });
+}
+
+
+// Just for debugging so we can see the output
+let updateDebug = (res) => {
+    this.document.getElementById('debug').innerHTML = JSON.stringify(res);
+}
+
+// Updates the UI
+// Call updateUI(trust, author, color) with values
+let updateUI = (trust, author, color) => {
+    // Update text
+    this.document.getElementById('trust-score').innerHTML = trust;
+    this.document.getElementById('author-score').innerHTML = author;
+
+    // Update color
+    let gradient = this.document.getElementById('gradient');
+    gradient.style.backgroundColor = color;
     gradient.style.background = 'linear-gradient' + '(to right,' + color + ', #fef9ee)';
-    this.document.getElementById('trust-score').innerHTML = this.score;
-    this.document.getElementById('author-score').innerHTML = this.getTrustScore();
+}
 
+// Promise for data
+// dataPromise.then(data => use(data))
+let dataPromise = (page) => {
+    return new Promise((resolve, reject) => {
+        fetch(ENDPOINT_URL + page)
+        .then(res => {
+            if(!res.ok) reject("error"); 
+            else return res})
+        .then(res => res.json())
+        .then(res => resolve(res));
+    })
 }
