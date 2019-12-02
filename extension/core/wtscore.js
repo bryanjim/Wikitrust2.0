a50277dbb47ec6e8c6926b789a3c0b8a8a037382
Joseph Csoti <josephcsoti@gmail.com>
2019-12-01 21:11:55 -0800

    connected db to new ui

--- extension/core/wtscore.js
+++ extension/core/wtscore.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 const ENDPOINT_URL = 'https://firestore.googleapis.com/v1/projects/wikitrustapp/databases/(default)/documents/articles/'
 
 let colorPercentage = (percentage) => {
-=======
-function colorPercentage(percentage) {
->>>>>>> 0d8363e6d43040b8404e6c6964255992a19764c6
     let r, g, b = 0;
     if (percentage < 50) {
         r = 225;
@@ -18,7 +14,6 @@ function colorPercentage(percentage) {
     return '#' + ('000000' + hex.toString(16)).slice(-6);
 }
 
-<<<<<<< HEAD
 // Entry point of app
 window.onload = function(){
     loadDummyScores();
@@ -32,8 +27,8 @@ let loadDummyScores = () => {updateUI("Loading...", "Loading...", colorPercentag
 let loadRealScores = (page) => {
     dataPromise(page)
         .then((res) => {
-            let trust = 101;
-            let author = 101;
+            let trust = 96;
+            let author = 96;
             let color = colorPercentage(trust)
             updateUI(trust, author, color);
             updateDebug(res);
@@ -49,15 +44,31 @@ let updateDebug = (res) => {
 // Updates the UI
 // Call updateUI(trust, author, color) with values
 let updateUI = (trust, author, color) => {
-    // Update text
-    this.document.getElementById('trust-score').innerHTML = trust;
-    this.document.getElementById('author-score').innerHTML = author;
 
-    // Update color
-    let gradient = this.document.getElementById('gradient');
-    gradient.style.backgroundColor = color;
-    gradient.style.background = 'linear-gradient' + '(to right,' + color + ', #fef9ee)';
+    // Change trust
+    let value = $('#trust-value')
+    value.html(trust);
+
+    // Change color
+    let left = $('.progress-left .progress-bar')
+    let right = $('.progress-right .progress-bar')
+
+    left.css("border-color", color);
+    right.css("border-color", color);
+
+
+    if(trust > 0){
+        if(trust <= 50){
+            right.css('transform', 'rotate(' + percentageToDegrees(trust) + 'deg)')
+        } else {
+            right.css('transform', 'rotate(180deg)')
+            left.css('transform', 'rotate(' + percentageToDegrees(trust - 50) + 'deg)')
+        }
+    }
 }
+
+// Transform to deg
+let percentageToDegrees = (percent) => {return percent / 100 * 360};
 
 // Promise for data
 // dataPromise.then(data => use(data))
@@ -71,42 +82,7 @@ let dataPromise = (page) => {
         .then(res => resolve(res));
     })
 }
-=======
-function getTrustScore() {
-    let trust = Math.floor(Math.random() * 100) + 1;
-    // This is just to test the background/border color using JS
-    return trust;
+
+let requestCrawlPromise = (page) => {
+    
 }
-
-
-$(function () {
-    let value = getTrustScore();
-    var trustColor = colorPercentage(value);
-
-    $(".progress").each(function () {
-        $(this).find('#trust-value').append(value);
-        var left = $(this).find('.progress-left .progress-bar');
-        var right = $(this).find('.progress-right .progress-bar');
-        left.css("border-color", trustColor);
-        right.css("border-color", trustColor);
-
-
-        if (value > 0) {
-            if (value <= 50) {
-                right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)')
-            } else {
-                right.css('transform', 'rotate(180deg)')
-                left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)')
-            }
-        }
-
-    })
-
-    function percentageToDegrees(percentage) {
-
-        return percentage / 100 * 360
-
-    }
-
-});
->>>>>>> 0d8363e6d43040b8404e6c6964255992a19764c6
